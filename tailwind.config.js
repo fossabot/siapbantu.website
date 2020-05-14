a94b93045133de54510678e17fc6fcc342e34988
Ahmad Ainul Rizki <human@shitiomatic.email>
2020-05-15 00:50:15 +0700

    Minor prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,144 +1,144 @@
-const plugin = require("tailwindcss/plugin");
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   theme: {
     extend: {
       colors: {
-        smoke: "rgba(0, 0, 0, 0.5)",
-        "indigo-50": "#F9F9FF"
+        smoke: 'rgba(0, 0, 0, 0.5)',
+        'indigo-50': '#F9F9FF',
       },
       maxHeight: {
-        "400px": "400px"
+        '400px': '400px',
       },
       minHeight: {
-        "400px": "400px"
+        '400px': '400px',
       },
       boxShadow: {
-        users: "0px 3px 16px rgba(0, 0, 0, 0.15)"
+        users: '0px 3px 16px rgba(0, 0, 0, 0.15)',
       }
     },
     container: {
-      center: true
+      center: true,
     },
     fontFamily: {
-      sans:
-        'Roboto, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
-    }
+      sans: 'Roboto, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    },
   },
   variants: {
-    translate: ["responsive", "hover", "focus", "group-hover"],
-    display: ["responsive", "hover", "focus", "group-hover"]
+    translate: ['responsive', 'hover', 'focus', 'group-hover'],
+    display: ['responsive', 'hover', 'focus', 'group-hover'],
   },
   plugins: [
-    require("@tailwindcss/ui"),
+    require('@tailwindcss/ui'),
     /*
-     * Spacing utilities
-     * Ex: .space-y-bottom-2 space-x-right-4
-     */
-    plugin(function({ addUtilities, theme }) {
-      const property = "margin";
-      const axes = ["x", "y"];
+    * Spacing utilities
+    * Ex: .space-y-bottom-2 space-x-right-4
+    */
+    plugin(function ({ addUtilities, theme }) {
+      const property = 'margin'
+      const axes = ['x', 'y']
       const directions = {
-        x: ["right", "left"],
-        y: ["top", "bottom"]
-      };
-      let utilities = {};
+        x: ['right', 'left'],
+        y: ['top', 'bottom'],
+      }
+      let utilities = {}
 
-      axes.forEach(axis => {
-        directions[axis].forEach(direction => {
-          const spacing = [];
+      axes.forEach((axis) => {
+        directions[axis].forEach((direction) => {
+          const spacing = []
 
-          Object.keys(theme("spacing")).filter(key => {
+          Object.keys(theme('spacing')).filter((key) => {
             // fetching only the absolute values (numbers, no 1/2, 3/5, etc.)
-            if (!key.includes("/")) {
-              spacing[key] = theme("spacing")[key];
+            if (!key.includes('/')) {
+              spacing[key] = theme('spacing')[key];
             }
-          });
+          })
 
-          Object.keys(spacing).forEach(key => {
-            const value = spacing[key];
-            const className = `.space-${axis}-${direction}-${key}`;
+          Object.keys(spacing).forEach((key) => {
+            const value = spacing[key]
+            const className = `.space-${axis}-${direction}-${key}`
 
-            const childrenProperties = {};
-            childrenProperties[`${property}-${direction}`] = value;
+            const childrenProperties = {}
+            childrenProperties[`${property}-${direction}`] = value
 
             const properties = {
-              ">*": childrenProperties
-            };
-            utilities[className] = properties;
-          });
-        });
-      });
+              '>*': childrenProperties,
+            }
+            utilities[className] = properties
+          })
+        })
+      })
 
-      addUtilities(utilities, { variants: ["responsive"] });
+      addUtilities(utilities, { variants: ['responsive'] })
     }),
-    plugin(function({ addUtilities, theme }) {
-      const utilities = {
-        ".grid-auto-row-1fr": {
-          "grid-auto-rows": "1fr"
-        }
-      };
+    plugin(function ({ addUtilities, theme }) {
 
-      addUtilities(utilities);
+      const utilities = {
+        '.grid-auto-row-1fr': {
+          'grid-auto-rows': '1fr'
+        }
+      }
+
+      addUtilities(utilities)
     }),
     // buttons
-    plugin(function({ addComponents, theme }) {
+    plugin(function ({ addComponents, theme }) {
       const styles = {
-        display: "inline-flex",
-        alignItems: "center",
-        fontSize: theme("fontSize.sm"),
-        padding: `${theme("spacing.2")} ${theme("spacing.4")}`,
-        fill: "currentColor",
-        whiteSpace: "nowrap",
-        backgroundColor: theme("colors.white"),
-        color: theme("colors.gray.900"),
-        border: `1px solid ${theme("colors.gray.100")}`,
-        transitionProperty: theme("transitionProperty.default"),
-        transitionDuration: theme("transitionDuration.100"),
-        borderRadius: theme("borderRadius.default"),
-        boxShadow: theme("boxShadow.default"),
-        "&:hover": {
-          backgroundColor: theme("colors.gray.100")
+        display: 'inline-flex',
+        alignItems: 'center',
+        fontSize: theme('fontSize.sm'),
+        padding: `${theme('spacing.2')} ${theme('spacing.4')}`,
+        fill: 'currentColor',
+        whiteSpace: 'nowrap',
+        backgroundColor: theme('colors.white'),
+        color: theme('colors.gray.900'),
+        border: `1px solid ${theme('colors.gray.100')}`,
+        transitionProperty: theme('transitionProperty.default'),
+        transitionDuration: theme('transitionDuration.100'),
+        borderRadius: theme('borderRadius.default'),
+        boxShadow: theme('boxShadow.default'),
+        '&:hover': {
+          backgroundColor: theme('colors.gray.100'),
         },
-        "&:active": {
-          backgroundColor: theme("colors.gray.200")
-        }
-      };
+        '&:active': {
+          backgroundColor: theme('colors.gray.200'),
+        },
+      }
       const buttons = {
-        "@variants responsive": {
-          ".button": {
+        '@variants responsive': {
+          '.button': {
             ...styles,
-            "&.button-lg": {
-              padding: `${theme("spacing.3")} ${theme("spacing.6")}`,
-              fontSize: theme("fontSize.lg")
+            '&.button-lg': {
+              padding: `${theme('spacing.3')} ${theme('spacing.6')}`,
+              fontSize: theme('fontSize.lg'),
             },
-            "&.button-sm": {
-              padding: `${theme("spacing.1")} ${theme("spacing.2")}`,
-              fontSize: theme("fontSize.sm")
+            '&.button-sm': {
+              padding: `${theme('spacing.1')} ${theme('spacing.2')}`,
+              fontSize: theme('fontSize.sm'),
             },
-            "&.button-xl": {
-              padding: `${theme("spacing.4")} ${theme("spacing.10")}`,
-              fontSize: theme("fontSize.2xl")
-            }
+            '&.button-xl': {
+              padding: `${theme('spacing.4')} ${theme('spacing.10')}`,
+              fontSize: theme('fontSize.2xl'),
+            },
           },
-          ".button-indigo": {
+          '.button-indigo': {
             ...styles,
-            color: theme("colors.white"),
-            backgroundColor: theme("colors.indigo.600"),
-            "&:hover": {
-              backgroundColor: theme("colors.indigo.700")
+            color: theme('colors.white'),
+            backgroundColor: theme('colors.indigo.600'),
+            '&:hover': {
+              backgroundColor: theme('colors.indigo.700'),
             },
-            "&:active": {
-              backgroundColor: theme("colors.indigo.800")
+            '&:active': {
+              backgroundColor: theme('colors.indigo.800'),
             },
-            "&1active": {
-              backgroundColor: theme("colors.indigo.800")
-            }
+            '&1active': {
+              backgroundColor: theme('colors.indigo.800'),
+            },
           }
-        }
-      };
+        },
+      }
 
-      addComponents(buttons);
+      addComponents(buttons)
     })
-  ]
-};
+  ],
+}
