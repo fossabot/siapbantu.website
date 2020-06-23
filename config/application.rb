a25ff23dd8897f12f348fe1d58ded56a7b309d8e
Ahmad Ainul Rizki <human@shitiomatic.email>
2020-06-24 00:47:25 +0700

    Asynchronous reporting

--- config/application.rb
+++ config/application.rb
@@ -24,35 +24,8 @@ end
 # Sentry DSN
 Raven.configure do |config|
   config.dsn = ENV['SENTRY_DSN']
-  config.async = false
-  config.context_lines = 3
-  config.encoding = 'gzip'
-  config.environments = []
-  config.exclude_loggers = []
-  config.excluded_exceptions = IGNORE_DEFAULT.dup
-  config.inspect_exception_causes_for_exclusion = false
-  config.linecache = ::Raven::LineCache.new
-  config.logger = ::Raven::Logger.new(STDOUT)
-  config.open_timeout = 1
-  config.processors = DEFAULT_PROCESSORS.dup
-  config.project_root = detect_project_root
-  config.rails_activesupport_breadcrumbs = false
-  config.rails_report_rescued_exceptions = true
-  config.release = detect_release
-  config.sample_rate = 1.0
-  config.sanitize_credit_cards = true
-  config.sanitize_fields = []
-  config.sanitize_fields_excluded = []
-  config.sanitize_http_headers = []
-  config.send_modules = true
-  config.server = ENV['SENTRY_DSN']
-  config.server_name = server_name_from_env
-  config.should_capture = false
-  config.ssl_verification = true
-  config.tags = {}
-  config.timeout = 2
-  config.transport_failure_callback = false
-  config.before_send = false
+  config.environments = %[production]
+  config.async = lambda { |event| SentryJob.perform_later(event) }
 end
 
 module CovidVolunteers
